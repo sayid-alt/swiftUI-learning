75f9981c40183aef5556a16194b25bdd0372f2ec
sayid-alt <heykalsayid@gmail.com>
2024-07-02 23:59:34 +0700

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Heykal Sayid on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LandmarkTests/LandmarkTests.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkTests.swift
+//  LandmarkTests
+//
+//  Created by Heykal Sayid on 02/07/24.
+//
+
+import Testing
+@testable import Landmark
+
+struct LandmarkTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
